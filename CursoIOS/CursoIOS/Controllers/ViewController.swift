1cdcb6254c1293f5ed4520fa9ef2485d925a0127
Paulo at Office <paulogaleano@gmail.com>
2019-09-24 20:35:52 +0200

    Imágenes de estudiantes y ejercicio de prueba

--- CursoIOS/CursoIOS/Controllers/ViewController.swift
+++ CursoIOS/CursoIOS/Controllers/ViewController.swift
@@ -11,11 +11,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelDemo: UILabel!
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    
+    @IBAction func onButton1Pressed(_ sender: UIButton) {
+        print("Button 1 pressed")
+        labelDemo.text = "Pulling up the courtains... hold on..."
+    }
+
+    @IBAction func onButton2Pressed(_ sender: UIButton) {
+        print("Button 2 pressed")
+            labelDemo.text = "Pulling down the courtains... hold on..."
+        }
+
+    @IBAction func onButton3Pressed(_ sender: UIButton) {
+        print("Button 3 pressed")
+        
+         labelDemo.text = "Turning on all lights at your home"
+    }
+    
+    @IBAction func onButton4Pressed(_ sender: UIButton) {
+        print("Button 4 pressed")
+        labelDemo.text = "Turning off all lights at your home"
+    }
+    
+    @IBAction func onSegmentControlChange(_ sender: UISegmentedControl) {
+        print("Se ha seleccionado el button")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        button1.setTitle("Pulling up all courtains in the house", for: .selected)
+        print("**********************Se ha entrado en viewDidLoad")
+
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+       
+        print("**********************Se ha entrado en viewWillAppear")
+    }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("**********************Se ha entrado en viewDidAppear")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("**********************Se ha entrado en viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("**********************Se ha entrado en viewDidDisappear")
+
+    }
 }
 
