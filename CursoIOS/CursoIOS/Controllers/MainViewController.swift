97a5eb5e324fb70c233ee9e8fc83fddb63786935
Paulo at Office <paulogaleano@gmail.com>
2019-09-24 20:58:02 +0200

    Preparación del Storyboard y el ViewController nuevos

---
+++ CursoIOS/CursoIOS/Controllers/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  CursoIOS
+//
+//  Created by Dev2 on 24/09/2019.
+//  Copyright © 2019 iZatasi. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("**********************Se ha entrado en viewDidLoad")
+
+    }
+    
+}
--- CursoIOS/CursoIOS/Controllers/viewControllerEjercicio.swift
+++
@@ -1,9 +0,0 @@
-//
-//  viewControllerEjercicio.swift
-//  CursoIOS
-//
-//  Created by Dev2 on 24/09/2019.
-//  Copyright © 2019 iZatasi. All rights reserved.
-//
-
-import Foundation
